2f48079a3c01cc1473f9a99eb8ef0eb0c1c1b3f6
mayssaGH <mayssagnm@gmail.com>
2025-04-06 19:54:40 +0100

    add tanstack table and make custom table components

---
+++ src/components/data-table/data-table.tsx
@@ -0,0 +1,159 @@
+"use client"
+
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    SortingState,
+    useReactTable,
+} from "@tanstack/react-table"
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/src/components/ui/table"
+import { useState } from "react"
+import { DataTableToolbar } from "@/src/components/data-table/table-toolbar"
+import { DataTablePagination } from "./table-pagination"
+import { useToast } from "@/src/hooks/use-toast"
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[],
+    data: TData[],
+    searchColumn: string,
+    filters?: {
+        column: string, values: {
+            label: string
+            value: string
+            icon?: React.ComponentType<{ className?: string }>
+        }[]
+    }[],
+    onRemoveSelected:  (ids: string[]) => Promise<{ success: boolean; message: string; }>
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data: initialData,
+    searchColumn,
+    filters,
+    onRemoveSelected,
+}: DataTableProps<TData, TValue>) {
+    const [data, setData] = useState<TData[]>(initialData);
+    const [sorting, setSorting] = useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+    const [rowSelection, setRowSelection] = useState({});
+    const {toast} = useToast();
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            columnFilters,
+            rowSelection,
+        },
+        meta: {
+            updateData: (rowIndex: number, columnId: string, value: string) => {
+                setData((old) =>
+                    old.map((row, index) => {
+                        if (index === rowIndex) {
+                            return {
+                                ...old[rowIndex],
+                                [columnId]: value,
+                            };
+                        }
+                        return row;
+                    })
+                );
+            },
+            removeRow: (rowIndex: number) => {
+                const setFilterFunc = (old: any) =>
+                    old.filter((_row: any, index: number) => index !== rowIndex);
+                setData(setFilterFunc);
+            },
+            removeSelectedRows: async () => {
+                const selectedRows = table.getSelectedRowModel().rows;
+                const selectedIndexes = selectedRows.map((row) => row.index);
+
+                const setFilterFunc = (old: any[]) =>
+                    old.filter((_row, index) => !selectedIndexes.includes(index));
+                const res = await onRemoveSelected(selectedRows.map((row) => row.original.id));
+                toast({
+                    description: res.message,
+                    variant: res.success ? "default" : "destructive"
+                });
+                if (res.success) {
+                    setData(setFilterFunc);
+                    table.resetRowSelection();
+                }
+
+            }
+        }
+    })
+
+    return (
+        <div className="space-y-4">
+            <DataTableToolbar table={table} searchColumn={searchColumn} filters={filters} />
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <DataTablePagination table={table} />
+        </div>
+    );
+}
---
+++ src/components/data-table/table-toolbar.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { Table } from "@tanstack/react-table"
+import { X } from "lucide-react"
+import { Input } from "../ui/input"
+import { DataTableFilter } from "./table-filter"
+import { Button } from "../ui/button"
+import { DataTableViewOptions } from "./table-column-toggle"
+
+interface DataTableToolbarProps<TData> {
+    table: Table<TData>,
+    searchColumn: string,
+    filters?: {
+        column: string, values: {
+            label: string
+            value: string
+            icon?: React.ComponentType<{ className?: string }>
+        }[]
+    }[],
+}
+
+export function DataTableToolbar<TData>({
+    table,
+    searchColumn,
+    filters,
+}: DataTableToolbarProps<TData>) {
+    const isFiltered = table.getState().columnFilters.length > 0
+
+    return (
+        <div className="flex items-center justify-between">
+            <div className="flex flex-1 items-center space-x-2">
+                <Input
+                    placeholder={`Filter ${searchColumn}s...`}
+                    value={(table.getColumn(searchColumn)?.getFilterValue() as string) ?? ""}
+                    onChange={(event: any) =>
+                        table.getColumn(searchColumn)?.setFilterValue(event.target.value)
+                    }
+                    className="h-8 w-[200px] lg:w-[300px]"
+                />
+                {filters?.map((filter, index) => (
+                    <DataTableFilter
+                        key={index}
+                        column={table.getColumn(filter.column)}
+                        title={filter.column}
+                        options={filter.values}
+                    />
+                ))}
+                {isFiltered && (
+                    <Button
+                        variant="ghost"
+                        onClick={() => table.resetColumnFilters()}
+                        className="h-8 px-2 lg:px-3"
+                    >
+                        Reset
+                        <X size={15} className="ml-2"/>
+                    </Button>
+                )}
+            </div>
+            <DataTableViewOptions table={table} />
+        </div>
+    )
+}
