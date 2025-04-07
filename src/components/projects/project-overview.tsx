58595789c36745e183cd2e4773b709bef6fecce2
mayssaGH <mayssagnm@gmail.com>
2025-04-07 12:30:21 +0100

    add external links in project overview

--- src/components/projects/project-overview.tsx
+++ src/components/projects/project-overview.tsx
@@ -2,11 +2,25 @@
 
 import { Project } from "@/src/types/models";
 import MDEditor from "@uiw/react-md-editor";
+import { Label } from "../ui/label";
+import { Card, CardContent, CardHeader } from "../ui/card";
 
 export function ProjectOverview({ project }: { project: Project }) {
     return (
+        <div className="flex flex-col lg:flex-row mt-8">
             <MDEditor.Markdown source={project.long_description} className="p-8" style={{ backgroundColor: "transparent" }} />
             {project.links &&
+                <Card className="my-12 bg-muted/50">
+                    <CardHeader><Label className="text-green text-md">External Links</Label></CardHeader>
+                    <CardContent>
+                        <ul className="min-w-64 list-disc text-sm pl-6 pt-2 underline decoration-green">
+                            {project.links.map((link, index) => (
+                                <li key={index} className="break-all" ><a href={link} className="break-all">{link}</a></li>
+                            ))}
+                        </ul>
+                    </CardContent>
+                </Card>
+            }
         </div>
     );
 }
