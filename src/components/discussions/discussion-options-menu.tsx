b216d98756546cfc75cb450624de90da1f87e15a
mayssaGH <mayssagnm@gmail.com>
2025-04-14 21:12:10 +0100

    add toasts after calling actions in discussion options menu and update actions accordingly

--- src/components/discussions/discussion-options-menu.tsx
+++ src/components/discussions/discussion-options-menu.tsx
@@ -6,11 +6,41 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem,
 import { Discussion } from "@/src/types/models";
 import { useRouter } from "next/navigation";
 import { CustomAlertDialog } from "../custom/alert-dialog";
-import { deleteDiscussion } from "@/src/utils/discussion-actions";
+import { deleteDiscussion, updateDiscussionStatus } from "@/src/utils/discussion-actions";
 import DiscussionFormDialog from "./discussion-form-dialog.tsx";
+import { DiscussionStatus } from "@/src/types/enums";
+import { useToast } from "@/src/hooks/use-toast";
 
-export function DiscussionDropdownMenu({ discussion }: { discussion: Discussion }) {
+export function DiscussionDropdownMenu({ discussion, showVisit = true }: { discussion: Discussion, showVisit?: boolean }) {
     const router = useRouter();
+    const { toast } = useToast();
+
+    const handleUpdateStatus = async (status: DiscussionStatus) => {
+        const res = await updateDiscussionStatus(discussion.id!, status);
+        toast({
+            description: res.message,
+            variant: res.success ? "default" : "destructive",
+        });
+
+        // Refresh the page if the user is on the discussion page
+        if (!showVisit) {
+            router.refresh();
+        }
+    }
+
+
+    const handleDelete = async () => {
+        const res = await deleteDiscussion(discussion.id!);
+        toast({
+            description: res.message,
+            variant: res.success ? "default" : "destructive",
+        });
+
+        // Redirect to user profile discussions page if deletion is successful
+        if (res.success) {
+            router.push("/profile/discussions");
+        }
+    }
 
     return (
         <DropdownMenu>
@@ -19,25 +49,47 @@ export function DiscussionDropdownMenu({ discussion }: { discussion: Discussion 
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-36">
                 <DropdownMenuGroup>
+                    {showVisit &&
+                        <DropdownMenuItem
+                            title="Visit discussion page"
+                            className="px-4"
+                            onClick={() => router.push(`/discussions/${discussion.id}`)}
+                        >
+                            Visit
+                        </DropdownMenuItem>
+                    }
+                    {/* for editing */}
                     <DropdownMenuItem
-                        className="px-4"
-                        onClick={() => router.push(`/discussions/${discussion.id}`)}
-                    >
-                        Visit
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
+                        title="Edit"
                         className="px-4"
                         onSelect={(event) => {
-                            event.preventDefault(); // Prevent menu from closing
+                            event.preventDefault(); // Prevent dialog from closing
+                            document.body.style.overflow = ""; // Restore scroll manually (when form dialog is closed focus is locked)
                         }}
                     >
                         <DiscussionFormDialog data={{ ...discussion, creator: discussion.creator.id }} />
                     </DropdownMenuItem>
+                    {discussion.status === DiscussionStatus.OPEN ?
+                        <DropdownMenuItem
+                            title="Do not allow new replies"
+                            className="px-4"
+                            onClick={() => handleUpdateStatus(DiscussionStatus.CLOSED)}
+                        >
+                            Close
+                        </DropdownMenuItem> :
+                        <DropdownMenuItem
+                            title="Allow new replies"
+                            className="px-4"
+                            onClick={() => handleUpdateStatus(DiscussionStatus.OPEN)}
+                        >
+                            Open
+                        </DropdownMenuItem>
+                    }
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                     onSelect={(event) => {
-                        event.preventDefault(); // Prevent menu from closing
+                        event.preventDefault(); // Prevent dialog from closing
                     }}
                     className="hover:text-destructive">
                     <CustomAlertDialog
@@ -47,7 +99,7 @@ export function DiscussionDropdownMenu({ discussion }: { discussion: Discussion 
                         title="Are you Sure ?"
                         description="This action cannot be undone."
                         confirmText="Delete discussion"
-                        onConfirm={() => deleteDiscussion(discussion.id!)}
+                        onConfirm={handleDelete}
                         buttonClass="h-full hover:text-destructive pl-0"
                     />
                 </DropdownMenuItem>
--- src/utils/discussion-actions.ts
+++ src/utils/discussion-actions.ts
@@ -1,18 +1,29 @@
 "use server";
 
 import { createClient } from "@/src/utils/supabase/server";
-import { encodedRedirect } from "./utils";
 import { deleteFromMinIO } from "./minio/client";
+import { DiscussionStatus } from "../types/enums";
 
 // Change later to soft delete
-export async function deleteDiscussion(id: string) {
+export async function updateDiscussionStatus(id: string, status: DiscussionStatus) {
     const supabase = await createClient();
-    const { error } = await supabase.from("discussion").delete().eq("id", id);
+    const { error } = await supabase.from("discussions").update({ status }).eq("id", id);
     if (error) {
         console.error("Database error:", error.message);
-        return encodedRedirect("error", "/profile/discussions", "Failed to delete discussion");
+        return { success: false, message: "Failed to update discussion status." };
+    }
+
+    return { success: true, message: "Discussion status updated successfully." };
+}
+
+export async function deleteDiscussion(id: string) {
+    const supabase = await createClient();
+    const { error } = await supabase.from("discussions").delete().eq("id", id);
+    if (error) {
+        console.error("Database error:", error.message);
+        return { success: false, message: "Failed to delete discussion." };
     }
     await deleteFromMinIO(`discussions/${id}`, true);
 
-    return encodedRedirect("success", "/profile/discussions", "Discussion deleted successfully!");
+    return { success: true, message: "Discussion deleted successfully." };
 }
