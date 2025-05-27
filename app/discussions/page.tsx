e22dad835afffad2b1b891edb2372356e72e6be1
mayssaGH <mayssagnm@gmail.com>
2025-05-27 18:42:23 +0100

    improve hero banner display

--- app/discussions/page.tsx
+++ app/discussions/page.tsx
@@ -46,12 +46,12 @@ export default async function Page(props: {
     return (
         <div className="w-full flex flex-col items-center">
             <HeroSection image="/images/bg-9.jpg" title="Discussions" subtitle="Join public conversations, explore ideas, and challenge perspectives">
-                <div className="text-right">
+                <div>
                     <DiscussionFormDialog />
                 </div>
             </HeroSection>
             <div className="w-full flex flex-col gap-8 items-center rounded-lg mt-6 p-4">
-                <h2 className="font-semibold text-lg">Choose a Category</h2>
+                <h2 className="font-semibold text-lg">Filter by Category</h2>
                 <CategoriesCarousel />
                 <TagsFilter allTags={allTags} />
                 <div className="w-full flex justify-between gap-8 border-b border-muted-foreground/30 p-8 rounded-t-lg">
--- src/components/custom/hero-section.tsx
+++ src/components/custom/hero-section.tsx
@@ -9,14 +9,16 @@ export function HeroSection({ image, title, subtitle, children }: { image: strin
                     src={image}
                     fill
                     alt="hero image"
-                    className="object-cover object-center opacity-40"
+                    className="object-cover object-center"
                 />
+                <div className="absolute inset-0 bg-gradient-to-r from-muted to-muted/50"></div>
+
             </div>
 
             <div className="relative flex flex-col justify-between w-full h-full text-foreground p-8">
-                <div className="mt-16">
-                    <h1 className="text-4xl font-bold text-primary">{title}</h1>
-                    <p className="text-muted-foreground">{subtitle}</p>
+                <div className="mt-16 max-w-3xl">
+                    <h1 className="text-4xl font-bold text-primary mb-4">{title}</h1>
+                    <p className="text-muted-foreground/80 text-lg font-medium">{subtitle}</p>
                 </div>
                 {children}
             </div>
