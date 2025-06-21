934ac078cd8d42610d24128ef05602cbb9298a20
mayssaGH <mayssagnm@gmail.com>
2025-06-21 17:35:27 +0100

    add missing ReportsGroupActions call in reports datatable

--- src/components/admin/reports/reports-table.tsx
+++ src/components/admin/reports/reports-table.tsx
@@ -2,7 +2,7 @@ import { DataTable } from "@/src/components/data-table/data-table";
 import { fetchAllReports } from "@/src/lib/services/admin-service";
 import { reportsTableColumns } from "./reports-table-columns";
 import { reportFilters } from "./reports-table-filters";
-import { deleteReports } from "@/src/lib/actions/admin/reports-actions";
+import { ReportsGroupActions } from "./reports-table-group-actions";
 
 
 export default async function ReportsTable() {
@@ -15,8 +15,8 @@ export default async function ReportsTable() {
                 data={reports}
                 searchColumn="reporter"
                 filters={reportFilters}
-                onRemoveSelected={deleteReports}
             >
+                <ReportsGroupActions/>
             </DataTable>
         </div>
     );
