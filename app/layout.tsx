845046606e5c6a23f19d886ad823aecd4a96da27
mayssaGH <mayssagnm@gmail.com>
2025-07-17 19:46:03 +0100

    add google site verification in meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClientThemeProvider } from "@/src/components/wrappers/client-theme-prov
 import { Toaster } from "@/src/components/ui/toaster";
 import { NotificationProvider } from "@/src/contexts/notification-context";
 import { Analytics } from '@vercel/analytics/next';
+import Head from "next/head";
 import { Metadata } from "next";
 import { siteMetadata } from "@/src/data/siteMetadata";
 
@@ -22,6 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={orbitron.className}>
+      <Head>
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="VKAQGan9q_6iEKNQLdrPNHMacZ2_0LzQBs2hB5i88v0" />
+      </Head>
       <body className="h-full bg-background text-foreground">
         <ClientThemeProvider>
           <AuthProvider>
